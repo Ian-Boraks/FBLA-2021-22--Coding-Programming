aaaf778062e77684383cbc7bbae79397e959a51c
IanBoraks <ianboraks@gmail.com>
2022-01-08 13:04:49 -0700

    Fixed custom to be simpler

--- assets/js/navigation_menu.js
+++ assets/js/navigation_menu.js
@@ -10,9 +10,8 @@ let navigationValues = {
 
 // TODO: Make this better with a drop down menu
 window.customUpdateNavigation = function () {
-  let type = prompt('Enter a type from the list below or click cancle to only enter a keyword:\naccounting\nairport\namusement_park\naquarium\nart_gallery\natm\nbakery\nbank\nbar\nbeauty_salon\nbicycle_store\nbook_store\nbowling_alley\nbus_station\ncafe\ncampground\ncar_dealer\ncar_rental\ncar_repair\ncar_wash\ncasino\ncemetery\nchurch\ncity_hall\nclothing_store\nconvenience_store\ncourthouse\ndentist\ndepartment_store\ndoctor\ndrugstore\nelectrician\nelectronics_store\nembassy\nfire_station\nflorist\nfuneral_home\nfurniture_store\ngas_station\ngym\nhair_care\nhardware_store\nhindu_temple\nhome_goods_store\nhospital\ninsurance_agency\njewelry_store\nlaundry\nlawyer\nlibrary\nlight_rail_station\nliquor_store\nlocal_government_office\nlocksmith\nlodging\nmeal_delivery\nmeal_takeaway\nmosque\nmovie_rental\nmovie_theater\nmoving_company\nmuseum\nnight_club\npainter\npark\nparking\npet_store\npharmacy\nphysiotherapist\nplumber\npolice\npost_office\nprimary_school\nreal_estate_agency\nrestaurant\nroofing_contractor\nrv_park\nschool\nsecondary_school\nshoe_store\nshopping_mall\nspa\nstadium\nstorage\nstore\nsubway_station\nsupermarket\nsynagogue\ntaxi_stand\ntourist_attraction\ntrain_station\ntransit_station\ntravel_agency\nuniversity\nveterinary_care\nzoo\n');
-  let keyword = prompt('Enter any keyword/s to search for or click cancle to only use a type');
-  updateNavigation('typeArray', type ? [type] : []);
+  let keyword = prompt('Enter a keyword to search for:');
+  updateNavigation('typeArray', []);
   updateNavigation('keyword', keyword ? keyword : '');
 }
 
